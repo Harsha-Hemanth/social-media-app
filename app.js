bb9fe9f0f756b6084bb219d9afe84b52ff7c06fb
Harsha-Hemanth <158434803+Harsha-Hemanth@users.noreply.github.com>
2024-12-10 15:30:18 +0530

    routing HTML

--- app.js
+++ app.js
@@ -49,8 +49,12 @@ app.use(session({ secret: SECRET_KEY, resave: false, saveUninitialized: true, co
     }
   }
 
-// routing HTML.
-
+  app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+  app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'public', 'register.html')));
+  app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'public', 'login.html')));
+  app.get('/post', requireAuth, (req, res) => res.sendFile(path.join(__dirname, 'public', 'post.html')));
+  app.get('/index', requireAuth, (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html'), { username: req.user.username }));
+  
 // user registration.
 
 // user login.
